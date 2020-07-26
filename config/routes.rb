dbc09acb122a983dd19edf4c5dcf0096d352e3dd
kinebuchi_kazuki <kinebuchi_kazuki@kinebuchikazukinoMacBook-Air.local>
2020-07-26 21:36:59 +0900

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get 'users/new', as: :signup
-
   root 'static_pages#home'
-  
   get 'static_pages/help', as: :help
   get 'static_pages/about', as: :about
   get 'static_pages/contact', as: :contact
+
+  get 'users/new', as: :signup
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
