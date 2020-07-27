79766f7501c353fda22a3412178b97b4fbed6ce1
kinebuchi_kazuki <kinebuchi_kazuki@kinebuchikazukinoMacBook-Air.local>
2020-07-27 18:41:52 +0900

    add session

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get 'static_pages/help', as: :help
-  get 'static_pages/about', as: :about
-  get 'static_pages/contact', as: :contact
+  get 'static_pages/help' => "static_pages#help", as: :help
+  get 'static_pages/about' => "static_pages#about", as: :about
+  get 'static_pages/contact' => "static_pages#contact", as: :contact
 
-  get 'users/new', as: :signup
+  get 'signup' => "users#new", as: :signup
+
+  get 'login' => "sessions#new" , as: :sessions_new
+  post 'login' => "sessions#create", as: :login
+  delete 'logout' => "sessions#destroy", as: :logout
+
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
