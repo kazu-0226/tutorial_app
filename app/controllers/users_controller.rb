8cab2ed574ff002328ee03f93842bc023475d9ae
kinebuchi_kazuki <kinebuchi_kazuki@kinebuchikazukinoMacBook-Air.local>
2020-07-29 23:23:11 +0900

    Implement advanced login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,28 +1,35 @@
 class UsersController < ApplicationController
-  def new
-    @user = User.new
+  
+   # GET /users/:id
+   def show
+    @user = User.find(params[:id])
+    # => app/views/users/show.html.erb
+    # debugger
   end
 
+  def new
+    @user = User.new
+    # => form_for @user
+  end
+
+  # POST /users
   def create
     @user = User.new(user_params)
-    if @user.save
+    if @user.save # => Validation
+      # Sucess
       log_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
+      # GET "/users/#{@user.id}" => show
     else
-      render :new
+      # Failure
+      render 'new'
     end
   end
 
-  def show
-    @user = User.find(params[:id])
-  end
-
-
-  private
-
   def user_params
-    params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
+    params.require(:user).permit(
+      :name, :email, :password,
+      :password_confirmation)
   end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,20 +1,46 @@
 module SessionsHelper
-    def log_in(user)                # login_inメソッドにuser(ログイン時にユーザーが送ったメールとパス)を引数として渡す
-        session[:user_id] = user.id # ユーザーidをsessionのuser_idに代入（ログインidの保持）
-      end
+      # 渡されたユーザーでログインする
+  def log_in(user)
+    session[:user_id] = user.id
+  end
 
-    def current_user
-      if session[:user_id] # ログインユーザーがいればtrue処理
-          @current_user ||= User.find_by(id: session[:user_id]) # ログインユーザーがいればそのまま、いなければcookiesのユーザーidと同じidを持つユーザーをDBから探して@current_user（現在のログインユーザー）に代入
+  # ユーザーを永続的に復元できるようになった
+  def remember(user)
+    user.remember # => DB: remember_digest
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
+  def current_user
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+
+    elsif (user_id = cookies.signed[:user_id])
+      # raise
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
       end
     end
+  end
 
-    def logged_in?
-      !current_user.nil? # current_user(ログインユーザー)がnilじゃないならtrue、それ以外ならfalseを返す
-    end
-    
-    def log_out
-      session.delete(:user_id)
-      @current_user = nil
-    end
+  def logged_in?
+    !current_user.nil?
+  end
+
+   # 現在のユーザーをログアウトする
+  def log_out
+    forget(current_user)
+    session.delete(:user_id)
+    @current_user = nil
+  end
+
+
 end
