ba0fa154a4e5e6aec549c979e2611d35423e847c
kinebuchi_kazuki <kinebuchi_kazuki@kinebuchikazukinoMacBook-Air.local>
2020-07-29 17:16:33 +0900

    Implement basic login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,10 @@ class SessionsController < ApplicationController
   end
 
   def create
-      user = User.find_by(email: params[:session][:email].downcase)
-      if user && user.authenticate(params[:session][:password])
-        log_in user
-        redirect_to user
+      user = User.find_by(email: params[:session][:email].downcase) # paramsハッシュで受け取ったemail値を小文字化し、email属性に渡してUserモデルから同じemailの値のUserを探して、user変数に代入
+      if user && user.authenticate(params[:session][:password]) # user変数がデータベースに存在し、なおかつparamsハッシュで受け取ったpassword値と、userのemail値が同じ(パスワードとメールアドレスが同じ値であれば)true
+        log_in user # sessions_helperのlog_inメソッドを実行し、sessionメソッドのuser_id（ブラウザに一時cookiesとして保存）にidを送る
+        redirect_to user #ログインしたuser(id)へ遷移→user_url(user)
       else
           flash.now[:danger] = 'Invalid email/password combination'
           render :new
@@ -15,6 +15,8 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-      
+    log_out
+    redirect_to root_url
   end
+
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,16 +1,20 @@
 module SessionsHelper
-    def log_in(user)
-        session[:user_id] = user.id
+    def log_in(user)                # login_inメソッドにuser(ログイン時にユーザーが送ったメールとパス)を引数として渡す
+        session[:user_id] = user.id # ユーザーidをsessionのuser_idに代入（ログインidの保持）
       end
 
     def current_user
-      if session[:user_id]
-          @current_user ||= User.find_by(id: session[:user_id])
+      if session[:user_id] # ログインユーザーがいればtrue処理
+          @current_user ||= User.find_by(id: session[:user_id]) # ログインユーザーがいればそのまま、いなければcookiesのユーザーidと同じidを持つユーザーをDBから探して@current_user（現在のログインユーザー）に代入
       end
     end
 
     def logged_in?
-      !current_user.nil?
+      !current_user.nil? # current_user(ログインユーザー)がnilじゃないならtrue、それ以外ならfalseを返す
     end
     
+    def log_out
+      session.delete(:user_id)
+      @current_user = nil
+    end
 end
