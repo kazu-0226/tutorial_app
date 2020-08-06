ae793fc746dfae1173543ccefef5421be7658e5d
kinebuchi_kazuki <kinebuchi_kazuki@kinebuchikazukinoMacBook-Air.local>
2020-08-06 19:32:49 +0900

    Add password reset

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    attr_accessor :remember_token, :activation_token                              # 記憶トークンと有効化トークンを定義
+    attr_accessor :remember_token, :activation_token     , :reset_token                         # 記憶トークンと有効化トークンを定義
     before_save { email.downcase! }                                               #DB保存前にemailの値を小文字に変換する
     before_create :create_activation_digest                                       # 作成前に適用
     validates :name,  presence: true, length: { maximum:  50 }                    #nameの文字列が空でなく、50文字以下ならtrue
@@ -50,6 +50,22 @@ class User < ApplicationRecord
       UserMailer.account_activation(self).deliver_now
     end
 
+        # パスワード再設定の属性を設定する
+    def create_reset_digest
+      self.reset_token = User.new_token
+      update_attribute(:reset_digest,  User.digest(reset_token))
+      update_attribute(:reset_sent_at, Time.zone.now)
+    end
+
+    # パスワード再設定のメールを送信する
+    def send_password_reset_email
+      UserMailer.password_reset(self).deliver_now
+    end
+
+    # パスワード再設定の期限が切れている場合はtrueを返す
+    def password_reset_expired?
+      reset_sent_at < 2.hours.ago
+    end
 
   private
 
