411c395b70eb95235fe7b2169a79c318ec705ebf
kinebuchi_kazuki <kinebuchi_kazuki@kinebuchikazukinoMacBook-Air.local>
2020-08-08 16:46:02 +0900

    Add user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ApplicationRecord
     has_many :microposts, dependent: :destroy
+
+    has_many :active_relationships, class_name:  "Relationship",foreign_key: :follower_id, dependent: :destroy
+    has_many :passive_relationships,class_name: "Relationship",foreign_key: :followed_id, dependent: :destroy
+    has_many :following, through: :active_relationships, source: :followed
+    has_many :followers, through: :passive_relationships, source: :follower
+
     attr_accessor :remember_token, :activation_token     , :reset_token                         # 記憶トークンと有効化トークンを定義
     before_save { email.downcase! }                                               #DB保存前にemailの値を小文字に変換する
     before_create :create_activation_digest                                       # 作成前に適用
@@ -72,6 +78,19 @@ class User < ApplicationRecord
   # 完全な実装は次章の「ユーザーをフォローする」を参照
   def feed
     Micropost.where("user_id = ?", id)
+  end
+
+  # ユーザーをフォローする
+  def follow(other_user)
+    following << other_user
+  end
+  # ユーザーをフォロー解除する
+  def unfollow
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+  # 現在のユーザーがフォローしてたらtrueを返す
+  def following?(other_user)
+    following.include?(other_user)
   end
 
 
